b40b6b8f688604cd5b101bb1077b4edefe2da127
Bernardo Dill <bernardodill10@gmail.com>
2026-04-29 11:42:17 +0000

    imagedata2 declarada

--- js/initCanvas.js
+++ js/initCanvas.js
@@ -1,12 +1,12 @@
-	let canvas = document.getElementById("quadro"); //canvas  layer 1
+	export let canvas = document.getElementById("quadro"); //canvas  layer 1
 
-	let canvas2 = document.getElementById("quadroFerramentas"); //canvas layer 2
-
-
-	let ctx = canvas.getContext('2d',{willReadFrequently: true}); //canvas layer 1
+	export let canvas2 = document.getElementById("quadroFerramentas"); //canvas layer 2
 
 
-	let ctxTools = canvas2.getContext('2d',{willReadFrequently: true}); //canvas layer 2
+	export let ctx = canvas.getContext('2d',{willReadFrequently: true}); //canvas layer 1
+
+
+	export let ctxTools = canvas2.getContext('2d',{willReadFrequently: true}); //canvas layer 2
 
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = window.innerHeight;
@@ -15,15 +15,15 @@
   	ctxTools.canvas.width  = window.innerWidth;
   	ctxTools.canvas.height = window.innerHeight;
 
-	let iniX=0;
-	let iniY=0;
-	let finalX=0;
-	let finalY=0;
-	let w = canvas.width;
-	let h = canvas.height;
-	let imageData = ctx.getImageData(0,0,w,h);
+	export let iniX=0;
+	export let iniY=0;
+	export let finalX=0;
+	export let finalY=0;
+	export let w = canvas.width;
+	export let h = canvas.height;
+	export let imageData = ctx.getImageData(0,0,w,h);
 	
-	let arrayData = [imageData];
+	export let arrayData = [imageData];
 	
 
 
@@ -32,4 +32,4 @@
 	ctx.fillRect(0, 0, w, h);
 	imageData = ctx.getImageData(0,0,w,h);
 
-	imageData2 = ctxTools.getImageData(0,0,canvas2.width,canvas2.height);
+	export let imageData2 = ctxTools.getImageData(0,0,canvas2.width,canvas2.height);
