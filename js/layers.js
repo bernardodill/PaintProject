65287a0117e46a3eba6cfb7a87aa6908fd99b35f
Bernardo <bernardodill10@gmail.com>
2026-05-04 19:00:38 -0300

    firstLayer

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
-firstCanvas = "<canvas class='layer1' style='border:1px solid #a7a7a7; background-color: #ffffff; width: 100vw; height: 100vh; margin-top: 100px; display: block;'></canvas>";
+
 
 $(".workspace").ready(function(){
-    $(".workspace").append(firstCanvas);
+    
 })
