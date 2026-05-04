9c3c1eae877a4e238307c3c04d4aa6e5282beca9
Bernardo <bernardodill10@gmail.com>
2026-05-04 20:39:51 -0300

    adicionado layerList e canvas List dinamicos

--- js/initCanvas.js
+++ js/initCanvas.js
@@ -1,6 +1,6 @@
-firstCanvas = "<canvas class='layer1' style='border:1px solid #a7a7a7; background-color: #ffffff; width: 100vw; height: 100vh; margin-top: 100px; display: block;'></canvas>";
+firstCanvas = "<canvas class='canvas1' style='border:1px solid #a7a7a7; background-color: #ffffff; width: 100vw; height: 100vh; margin-top: 100px; display: block;'></canvas>";
 
-$(document).ready(function() {
-    $('.layerList').append('<li class="layer 1">Layer 1</li>');
+$(".workspace").ready(function() {
+    $('.layerList').append('<li class="layer1">Layer 1</li>');
     $(".workspace").append(firstCanvas);
 });
--- js/layers.js
+++ js/layers.js
@@ -1,3 +1,10 @@
-$(".workspace").ready(function(){
-    
-})
+function addLayer(){
+    layerListLenght = $(".layerList li").length;
+
+    layerName = "layer"+ (layerListLenght + 1);
+
+    canvasName = "canvas"+ (layerListLenght + 1);
+
+    $(".layerList").append("<li class="+(layerName)+ ">Layer " + ($(".layerList li").length + 1) + "</li>");
+    $(".workspace").append("<canvas class='" + canvasName + "' style='border:1px solid #a7a7a7; background-color: transparent; width: 100vw; height: 100vh; margin-top: 100px; display: block;'></canvas>");
+}
