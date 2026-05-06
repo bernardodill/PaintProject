c1748cce9fb5557405aa3ccaa4e9e01758ec9a0a
Bernardo <bernardodill10@gmail.com>
2026-05-06 05:30:12 -0300

    working canvas

--- js/initCanvas.js
+++ js/initCanvas.js
@@ -1,10 +1,24 @@
-firstCanvas = "<canvas class='canvas1' style='border:1px solid #a7a7a7; background-color: #ffffff; width: 100vw; height: 100vh; margin-top: 100px; display: block;'></canvas>";
+firstCanvas = "<canvas id='canvas1' style='border:2px solid #a7a7a7; background-color: #ffffff; width: 100vw; height: 80vh; margin-top: 100px; display: block;position:absolute; bottom:0px'></canvas>";
 
-$(".workspace").ready(function() {
+
     $('.layerList').append('<li class="layer1">Layer 1</li>');
-    $(".workspace").append(firstCanvas);
-});
 
-    let canvas = $(".canvas1")[0];
-    console.log(canvas);
-    firstCanvas = canvas.getContext("2d", willReadFrequently = true);
+   // $(".workspace").html(firstCanvas);
+    $(".workspace").append(firstCanvas);
+
+
+    window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('canvas1'); // Ensure this ID matches your HTML
+    
+        console.log($("canvas"))
+        
+
+    if (canvas) {
+        let ctx = canvas.getContext('2d', { willReadFrequently: true });
+        ctx.beginPath();
+        ctx.fillStyle = "#b64545";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    } else {
+        console.error("Canvas element not found");
+    }
+});
