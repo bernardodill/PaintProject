a20ac7352ee0f7aa0516826d4f1364d552412058
Bernardo Dill <bernardodill10@gmail.com>
2026-05-07 12:39:12 +0000

    initCanvas

--- js/initCanvas.js
+++ js/initCanvas.js
@@ -1,20 +1,13 @@
-let firstCanvas = "<canvas id='canvas1' width='800' height='1200' style='border:1px solid #555555; display:block; background-color: #ffffff; position:absolute; top:2%; left:20%; z-index:1; margin:auto;'></canvas>";
+    let firstCanvas = "<canvas id='canvas1' width='800' height='1200' style='border:1px solid #555555; display:block; background-color: #ffffff; position:absolute; top:2%; left:20%; z-index:1; margin:auto;'></canvas>";
 
-
-    $('.layerList').append('<li  onclick=selectLayer(this) class="layer1">Layer 1</li>');
+    $('.layerList').append('<li onclick=selectLayer(this) class="layer1">Layer 1</li>');
 
     $(".workspace").append(firstCanvas);
 
     let windowWidth = window.innerWidth;
     let windowHeight = window.innerHeight;
-
     
-    window.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('canvas1'); 
+   
         
-    if (canvas) {
-
-    } else {
-        console.error("Canvas element not found");
-    }
+    
 });
