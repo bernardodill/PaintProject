b302863268ae251263f89b5614156d6f280a6f2a
Bernardo Dill <bernardodill10@gmail.com>
2026-05-07 12:18:16 +0000

    selectedLayer

--- js/initCanvas.js
+++ js/initCanvas.js
@@ -1,7 +1,7 @@
-let firstCanvas = "<canvas id='canvas1' width='800' height='1200' style='border:1px solid #555555; display:block; background-color: #ffffff; position:absolute; top:0; left:20%;  bottom:0; margin:auto;'></canvas>";
+let firstCanvas = "<canvas id='canvas1' width='800' height='1200' style='border:1px solid #555555; display:block; background-color: #ffffff; position:absolute; top:2%; left:20%; z-index:1; margin:auto;'></canvas>";
 
 
-    $('.layerList').append('<li class="layer1">Layer 1</li>');
+    $('.layerList').append('<li  onclick=selectLayer(this) class="layer1">Layer 1</li>');
 
     $(".workspace").append(firstCanvas);
 
--- js/layers.js
+++ js/layers.js
@@ -1,10 +1,26 @@
 function addLayer(){
     layerListLenght = $(".layerList li").length;
 
+
+
     layerName = "layer"+ (layerListLenght + 1);
 
     canvasName = "canvas"+ (layerListLenght + 1);
 
-    $(".layerList").append("<li class="+(layerName)+ ">Layer " + ($(".layerList li").length + 1) + "</li>");
-    $(".workspace").append("<canvas id='" + canvasName + "' width='800' height='1200' style='border:1px solid #555555; display:block; background-color: #ffffff; position:absolute; top:0; left:20%;  bottom:0; margin:auto;'></canvas>");
+    $(".layerList").append("<li onclick='selectLayer(this)' class="+(layerName)+ ">Layer " + ($(".layerList li").length + 1) + "</li>");
+
+
+    $(".workspace").append("<canvas id='" + canvasName + "' width='800' height='1200' style='border:1px solid #555555; display:block; position:absolute; top:2%; left:20%;  margin:auto; z-index:"+ parseInt(layerListLenght +1)  +"';></canvas>");
+}
+
+
+function selectLayer(el){
+    
+    let className = el.getAttribute("class");
+
+    const index = className.at(-1); // retorna ultimo character = numero do layer
+
+    let selectedCanvas = document.getElementById("canvas"+index)
+    console.log(selectedCanvas)
+    return selectedCanvas;
 }
