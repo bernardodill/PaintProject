e4e2939f7ff50b44f574177966542bc8fa75570e
Bernardo Dill <bernardodill10@gmail.com>
2026-05-12 07:57:08 -0300

    Remove addLayer function
    Removed the addLayer function that was responsible for adding layers and canvases.

--- js/layers.js
+++ js/layers.js
@@ -1,23 +1,7 @@
 import * as globals from "./globals.js";
 
 
-function addLayer(){
-    layerListLenght = $(".layerList li").length;
 
-    layerName = "layer"+ (layerListLenght + 1);
-
-    canvasName = "canvas"+ (layerListLenght + 1);
-
-    $(".layerList").append("<li onclick='selectLayer(this)' id='"+(layerName)+ "'>Layer " + ($(".layerList li").length + 1) + "<a><i class='delete fa-solid fa-x'></i></a></li>");
-
-    $(".workspace").append("<canvas id='" + canvasName + "' width='800' height='1200' style='border:1px solid #555555; display:block; position:absolute; top:2%; left:20%;  margin:auto; z-index:"+ parseInt(layerListLenght +1)  +"';></canvas>");
-
-    
-    globals.contexts.push(document.getElementById(canvasName).getContext("2d",{willReadFrequently: true}));
-
-
-
-}
 
 
 
