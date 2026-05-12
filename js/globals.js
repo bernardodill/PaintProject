4358c196e811c2c52a1e3947737a7c88e97696ab
Bernardo Dill <bernardodill10@gmail.com>
2026-05-12 13:42:13 +0000

    fix

--- js/globals.js
+++ js/globals.js
@@ -1,12 +1,8 @@
-export  let contexts = [];
-export let  selectedCanvas = $("#canvas1");
+export let contexts = [];
+export let selectedCanvas = $("#canvas1");
 
-export function selectLayer(el){
-    
-    if($("li").hasClass("selected")){
-        selectedCanvas = getElementById("canvas"+el.id.at(-1));
-    }
-   
+export function setSelectedCanvas(canvas) {
+    selectedCanvas = $(canvas);
 }
 
 
