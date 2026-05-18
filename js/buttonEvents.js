0a3bb1bd55cbf971b580630f977539dbb980292c
Bernardo Dill <bernardodill10@gmail.com>
2026-05-18 11:19:33 +0000

    change Layer

--- js/buttonEvents.js
+++ js/buttonEvents.js
@@ -1,17 +1,19 @@
 import * as globals from "./globals.js";
 
+
+
 // Função para aumentar ou diminuir o valor do input de tamanho da ferramenta
 function changeSize(event){
-    size = parseInt($(".size").val(),10);
+    size = parseInt(document.querySelector(".size").value,10);
     console.log(size);
 
-    if($(event).hasClass("numberUp")){
+    if(event.classList.contains("numberUp")){
         if(size < 10){
-            $(".size").val(parseInt($(".size").val(),10) + 1);
+            document.querySelector(".size").value = parseInt(document.querySelector(".size").value,10) + 1;
         }
-    } else if($(event).hasClass("numberDown")){
+    } else if(event.classList.contains("numberDown")){
         if(size > 1){
-            $(".size").val(parseInt($(".size").val(),10) - 1);
+            document.querySelector(".size").value = parseInt(document.querySelector(".size").value,10) - 1;
         }
     }
 }
@@ -29,9 +31,28 @@ addLayer.addEventListener("click", function (){
 
     let layerName = "layer"+(globals.layerListLenght + 1);
     
-    $(".layerList").append("<li class='layer' id='"+(layerName)+ "'>Layer " + ($(".layerList li").length + 1) + "<a><i class='delete fa-solid fa-x'></i></a></li>");
+    const layerListUl = document.querySelector(".layerList");
+    const newLayerLi = document.createElement("li");
+    newLayerLi.className = "layer";
+    newLayerLi.id = layerName;
+    newLayerLi.innerHTML = "Layer " + (layerListUl.querySelectorAll("li").length + 1) + "<a><i class='delete fa-solid fa-x'></i></a>";
+    layerListUl.appendChild(newLayerLi);
 
-    $(".workspace").append("<canvas id='" + canvasName + "' width='800' height='1200' style='border:1px solid #555555; display:block; position:absolute; top:2%; left:20%;  margin:auto; z-index:"+ parseInt(globals.layerListLenght +1)  +"';></canvas>");
+
+    let canvasName = "canvas"+(globals.layerListLenght + 1);
+
+    const newCanvas = document.createElement("canvas");
+    newCanvas.id = canvasName;
+    newCanvas.width = 800;
+    newCanvas.height = 1200;
+    newCanvas.style.border = "1px solid #555555";
+    newCanvas.style.display = "block";
+    newCanvas.style.position = "absolute";
+    newCanvas.style.top = "2%";
+    newCanvas.style.left = "20%";
+    newCanvas.style.margin = "auto";
+    newCanvas.style.zIndex = parseInt(globals.layerListLenght + 1);
+    document.querySelector(".workspace").appendChild(newCanvas);
 
 
     
@@ -41,24 +62,19 @@ addLayer.addEventListener("click", function (){
 
 
 
+//seleciona ol. elemento pai do li
+let listLayer = document.getElementsByClassName("layerList")[0];
 
-let changeSelectedLayer = document.getElementsByClassName("layer selected").item(0);
 
-console.log(changeSelectedLayer);
+listLayer.addEventListener("click", function(el){
 
-changeSelectedLayer.addEventListener("click", function(el){
+    if(el.target.tagName == "li"){
 
-    
-    let idName = el.getAttribute("id");
-
-    const index = idName.at(-1);
-
-    let selectedCanvas = document.getElementById("canvas"+index);
-    
-    console.log(selectedCanvas);
-    $("li.selected").removeClass("selected");
-    el.setAttribute("class","selected");
-    
-
-    //setSelectedCanvas(selectedCanvas);
+        console.log(el);
+        
+        document.getElementsByClassName("li selected").item(0).classList.remove("selected");
+        
+        el.setAttribute("class","selected");
+        
+    }
 });
